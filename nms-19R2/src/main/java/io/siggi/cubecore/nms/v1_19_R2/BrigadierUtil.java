be8cd841857c83a38135d39b3942be9413aa0813
Siggi <7286178+Siggi88@users.noreply.github.com>
2023-02-28 12:04:33 +0000

    Add support for up to 1.19.3

---
+++ nms-19R2/src/main/java/io/siggi/cubecore/nms/v1_19_R2/BrigadierUtil.java
@@ -0,0 +1,19 @@
+package io.siggi.cubecore.nms.v1_19_R2;
+
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import net.minecraft.commands.arguments.ArgumentChat;
+import net.minecraft.commands.synchronization.CompletionProviders;
+
+public class BrigadierUtil extends io.siggi.cubecore.nms.BrigadierUtil {
+
+    @Override
+    public ArgumentType argumentTypeChat() {
+        return ArgumentChat.a();
+    }
+
+    @Override
+    public SuggestionProvider suggestionProviderAskServer() {
+        return CompletionProviders.a;
+    }
+}
